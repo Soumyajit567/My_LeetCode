8124e0cfbc68b9ae8a859d4b6c642e140d690eee
Soumyajit Chakraborty <52927666+Soumyajit567@users.noreply.github.com>
2023-10-11 19:26:18 -0400

    Time: 1346 ms (9.55%), Space: 45.4 MB (16.12%) - LeetHub

--- 2334-number-of-flowers-in-full-bloom/2334-number-of-flowers-in-full-bloom.py
+++ 2334-number-of-flowers-in-full-bloom/2334-number-of-flowers-in-full-bloom.py
@@ -1,19 +1,31 @@
 class Solution:
     def fullBloomFlowers(self, flowers: List[List[int]], people: List[int]) -> List[int]:
-        starts = []
-        ends = []
-        
+        def binary_search(arr, target):
+            low, high = 0, len(arr) - 1
+            while low <= high:
+                mid = low + (high - low) // 2
+                if arr[mid] <= target:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+            return low  
+
+        # Prepare starts and ends lists as before
+        starts, ends = [], []
         for start, end in flowers:
             starts.append(start)
-            ends.append(end + 1)
-            
+            ends.append(end + 1)  # '+1' to handle the inclusive end time
         starts.sort()
         ends.sort()
+
+        # Initialize the answer list
         ans = []
 
+        # Process each person's arrival
         for person in people:
-            i = bisect_right(starts, person)
-            j = bisect_right(ends, person)
-            ans.append(i - j)
-        
+            start_count = binary_search(starts, person)
+            end_count = binary_search(ends, person)
+            # The number of flowers in bloom is the difference between those two counts
+            ans.append(start_count - end_count)
+
         return ans
