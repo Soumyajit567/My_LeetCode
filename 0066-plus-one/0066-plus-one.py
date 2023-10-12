8674d731cd4200513e44e202943117360d0ab1c5
Soumyajit Chakraborty <52927666+Soumyajit567@users.noreply.github.com>
2023-10-11 22:45:42 -0400

    Time: 39 ms (64.94%), Space: 16.2 MB (48.54%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,25 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        res = ""
+        if len(digits) == 1:
+            if digits[0] < 9:
+                return [1 + digits[0]]
+            else:
+                return [1, 0]
+        
+        else:
+            q = deque(digits)
+            while q:
+                val = q.popleft()
+                res += str(val)
+        num = int(res)
+        num += 1
+        string = str(num)
+        # print(res)
+        for k in range(len(string)):
+            if k < len(digits):
+                digits[k] = int(string[k])
+            else:
+                digits.append(int(string[k]))
+        return digits
+
