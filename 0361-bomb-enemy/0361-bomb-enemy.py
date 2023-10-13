4ba5c648f542398729cde3964f3c4af72b395f6d
Soumyajit Chakraborty <52927666+Soumyajit567@users.noreply.github.com>
2023-10-13 00:41:38 -0400

    Time: 675 ms (79.31%), Space: 21.5 MB (89.66%) - LeetHub

--- 0361-bomb-enemy/0361-bomb-enemy.py
+++ 0361-bomb-enemy/0361-bomb-enemy.py
@@ -6,20 +6,20 @@ class Solution:
         rows, cols = len(grid), len(grid[0])
 
         max_count = 0
-        row_hits = 0
+        row_hits = [0] * rows
         col_hits = [0] * cols
 
         for row in range(0, rows):
             for col in range(0, cols):
                 # reset the hits on the row, if necessary.
                 if col == 0 or grid[row][col - 1] == 'W':
-                    row_hits = 0
+                    row_hits[row] = 0
                     for k in range(col, cols):
                         if grid[row][k] == 'W':
                             # stop the scan when we hit the wall.
                             break
                         elif grid[row][k] == 'E':
-                            row_hits += 1
+                            row_hits[row] += 1
 
                 # reset the hits on the col, if necessary.
                 if row == 0 or grid[row - 1][col] == 'W':
@@ -32,7 +32,7 @@ class Solution:
 
                 # count the hits for each empty cell.
                 if grid[row][col] == '0':
-                    total_hits = row_hits + col_hits[col]
+                    total_hits = row_hits[row] + col_hits[col]
                     max_count = max(max_count, total_hits)
 
         return max_count
