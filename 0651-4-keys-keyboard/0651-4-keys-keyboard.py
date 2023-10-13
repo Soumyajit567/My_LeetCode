e57d22b846dc08b6c109b8355f54551d7086de9a
Soumyajit Chakraborty <52927666+Soumyajit567@users.noreply.github.com>
2023-10-13 17:35:50 -0400

    Time: 25 ms (99.48%), Space: 16.2 MB (58.33%) - LeetHub

--- 0651-4-keys-keyboard/0651-4-keys-keyboard.py
+++ 0651-4-keys-keyboard/0651-4-keys-keyboard.py
@@ -1,7 +1,17 @@
 class Solution:
+    @functools.cache
+    def maxA(self, n: int) -> int:
+        if n <= 5:
+            return n
+        else:
+            return max(n, max(i * self.maxA(n - i - 1) for i in [3, 4, 5, 6]))
+
+"""
+class Solution:
     def maxA(self, n: int) -> int:
         dp = list(range(n + 1))
         for i in range(n - 2):
             for j in range(i + 3, min(n, i + 6) + 1):
                 dp[j] = max(dp[j], (j - i - 1) * dp[i])
         return dp[n]
+"""
