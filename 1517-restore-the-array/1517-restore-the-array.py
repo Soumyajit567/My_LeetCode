a8f3b47db1f622a910c7571527464d2fb83f0a67
Soumyajit Chakraborty <52927666+Soumyajit567@users.noreply.github.com>
2023-10-13 23:30:13 -0400

    Time: 865 ms (88.85%), Space: 19.8 MB (88.85%) - LeetHub

--- 1517-restore-the-array/1517-restore-the-array.py
+++ 1517-restore-the-array/1517-restore-the-array.py
@@ -16,3 +16,36 @@ class Solution:
                 dp[i] = (dp[i] + dp[j + 1]) % MOD  # this substring is valid, add the number of ways from the rest of the string
         
         return dp[0]  # the result is the number of ways to print the array from the start of the string
+
+"""
+Let's consider s = "1317" and k = 2000 for clarity:
+
+We initialize our dp array with an extra space more than the length of s to handle the base case comfortably. So dp = [0, 0, 0, 0, 0] for n = 4 (length of "1317").
+
+The base case is when we have considered all characters in s, which can't form a number on their own, so dp[n] = 1 or dp[4] = 1. Now, dp = [0, 0, 0, 0, 1].
+
+We start our iteration from the end of the string. The outer loop's index i goes from n-1 to 0, i.e., from 3 to 0 in this case.
+
+In each iteration of i, we build the number num by iterating through the rest of the string starting from i to n-1 (using index j), and we break the inner loop if num exceeds k.
+
+Let's see how dp gets updated:
+
+For i = 3 ("7"): num will be 7, 17, 317, 1317 as j goes from 3 to 0. None exceed k, so we add dp[j+1] to dp[3] for each valid j, which gives dp[3] = 0 + 1 = 1.
+
+For i = 2 ("1"): num will be 1, 17, 317. None exceed k, so we add dp[j+1] to dp[2], which gives dp[2] = 0 + 1 + 1 = 2.
+
+For i = 1 ("3"): num will be 3, 31, 317. None exceed k, so we add dp[j+1] to dp[1], which gives dp[1] = 0 + 1 + 1 + 2 = 4.
+
+For i = 0 ("1"): num will be 1, 13, 131, 1317. None exceed k, so we add dp[j+1] to dp[0], which gives dp[0] = 0 + 1 + 1 + 2 + 4 = 8.
+
+So, the final dp array will be [8, 4, 2, 1, 1], and our answer is dp[0], which is 8.
+
+This represents the total number of ways you can partition the string "1317" into valid integers that don't exceed 2000. The "partitions" here aren't explicitly created but are represented by the different values that num takes as we iterate through s with i and j.
+
+
+
+
+
+
+
+"""
