2c7666722d0a524bbd27183e156900537c90fc75
Soumyajit Chakraborty <52927666+Soumyajit567@users.noreply.github.com>
2023-10-13 20:09:51 -0400

    Time: 43 ms (38.54%), Space: 16.3 MB (27.6%) - LeetHub

--- 0651-4-keys-keyboard/0651-4-keys-keyboard.py
+++ 0651-4-keys-keyboard/0651-4-keys-keyboard.py
@@ -1,5 +1,23 @@
 class Solution:
     def maxA(self, n: int) -> int:
+        @functools.cache
+        def dp(i):
+            if i <= 0:
+                return 0
+            max_count = dp(i - 1) + 1  # pressing 'A'
+            for j in range(1, i - 2):  # leave at least two steps for Ctrl-A and Ctrl-C, and one for Ctrl-V
+                # It's i - j - 1 because we're using i - j steps after j, and one of them is used for Ctrl-V.
+                max_count = max(max_count, dp(j) * (i - j - 1))  # Ctrl-A, Ctrl-C, followed by (i - j - 1) times Ctrl-V
+            return max_count
+
+        return dp(n)
+
+
+
+
+"""
+class Solution:
+    def maxA(self, n: int) -> int:
         dp = [0] * (n + 1)
     
         for i in range(1, n + 1):
@@ -10,6 +28,7 @@ class Solution:
                 dp[i] = max(dp[i], dp[j - 2] * (i - j + 1))
 
         return dp[n]
+"""
 
 """
 class Solution:
