d03d3a560b25eab4454a93828a93ff4b1e942289
Soumyajit Chakraborty <52927666+Soumyajit567@users.noreply.github.com>
2023-10-16 13:15:22 -0400

    Time: 904 ms (82.09%), Space: 27.2 MB (99.82%) - LeetHub

--- 1896-maximum-score-from-performing-multiplication-operations/1896-maximum-score-from-performing-multiplication-operations.py
+++ 1896-maximum-score-from-performing-multiplication-operations/1896-maximum-score-from-performing-multiplication-operations.py
@@ -20,31 +20,12 @@ class Solution:
 
 class Solution:
     def maximumScore(self, nums: List[int], multipliers: List[int]) -> int:
-        # Number of operations to be performed
-        m = len(multipliers)
-        # Length of the nums list
         n = len(nums)
+        m = len(multipliers)
+        dp = [[0 for x in range(m + 1)] for y in range(m + 1)]
 
-        # Initialize a 2D dp array with dimensions (m+1) x (m+1)
-        dp = [[0] * (m + 1) for _ in range(m + 1)]
-
-        # We start from the last operation and move to the first one
-        for op in range(m - 1, -1, -1):
-            # The 'left' index can go from 'op' to 0
-            for left in range(op, -1, -1):
-                # Calculate the 'right' index for taking an element from the end of 'nums'
-                # It's determined by how many elements are already taken from the start ('left')
-                # and how many operations have been performed ('op')
-                right = n - 1 - (op - left)  # Calculate the corresponding right index
-
-                # Update the dp table considering two choices: 
-                # taking the next element either from the start or the end of the 'nums' list
-                # The current operation 'op' is used to pick the multiplier, and the 'left' index
-                # is used to pick an element from the start of the 'nums' list.
-                dp[op][left] = max(
-                    multipliers[op] * nums[left] + dp[op + 1][left + 1],  # Choose from the start
-                    multipliers[op] * nums[right] + dp[op + 1][left]  # Choose from the end
-                )
-
-        # The answer is in dp[0][0] which considers all operations starting from 0
+        for i in range(m - 1, -1, -1):
+            for j in range(i, -1, -1):
+                right = n - 1 - (i - j) 
+                dp[i][j] = max(nums[j] * multipliers[i] + dp[i + 1][j + 1], nums[right] * multipliers[i] + dp[i + 1][j])
         return dp[0][0]
