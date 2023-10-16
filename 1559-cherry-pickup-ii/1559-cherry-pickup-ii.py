40c5fb2a43fb0aca6b17df9894ee040b3a8f7393
Soumyajit Chakraborty <52927666+Soumyajit567@users.noreply.github.com>
2023-10-16 16:46:44 -0400

    Time: 958 ms (75.74%), Space: 75.6 MB (5.88%) - LeetHub

--- 1559-cherry-pickup-ii/1559-cherry-pickup-ii.py
+++ 1559-cherry-pickup-ii/1559-cherry-pickup-ii.py
@@ -1,3 +1,52 @@
+from functools import cache
+
+class Solution:
+    def cherryPickup(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        # Remove the visited sets; they are not needed in this approach
+
+        # Change dp1 and dp2 into a single dictionary for memoization.
+        # This dictionary will hold the maximum cherries two robots can pick starting from (i, j) and (i, k) to the bottom.
+        dp = {}
+
+        @cache
+        def dfs(i, j, k):
+            # Check for out of bounds
+            if i == m or j < 0 or j == n or k < 0 or k == n:
+                return 0
+
+            # If this state is already computed
+            if (i, j, k) in dp:
+                return dp[(i, j, k)]
+
+            # Cherries at the current positions of both robots
+            # If they're at the same position, we shouldn't double-count the cherries.
+            cherries = grid[i][j] + (grid[i][k] if j != k else 0)
+
+            # Max cherries picked from this position to the end
+            max_cherries = 0
+            for dj in [1, 0, -1]:
+                for dk in [1, 0, -1]:
+                    # Consider all possible moves for both robots
+                    nj, nk = j + dj, k + dk
+                    max_cherries = max(max_cherries, dfs(i + 1, nj, nk))
+
+            # Cherries picked from this cell plus max cherries picked from future moves
+            answer = cherries + max_cherries
+
+            # Save to the memoization dictionary
+            dp[(i, j, k)] = answer
+            return answer
+
+        # Start the DFS from the top row, with robots at the leftmost and rightmost positions
+        return dfs(0, 0, n - 1)
+
+
+
+
+
+
+"""
 from functools import lru_cache
 
 class Solution:
@@ -26,3 +75,4 @@ class Solution:
 
         # We start from row 0 with Robot #1 at column 0 and Robot #2 at column n-1
         return dp(0, 0, n - 1)
+"""
