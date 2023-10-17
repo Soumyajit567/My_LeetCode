13a13a1229239d5b924ceb77be2044ea4c02cf2b
Soumyajit Chakraborty <52927666+Soumyajit567@users.noreply.github.com>
2023-10-17 19:14:23 -0400

    Time: 1312 ms (15.92%), Space: 46.9 MB (6.9%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,26 +1,21 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = {}  
-        @cache
+        dp = {}
+        n = len(coins)
         def memo(i):
-
             if i == amount:
                 return 0
-            if i > amount:
-                return -1
-            
-         
-            if i in dp:
+            elif i > amount:
+                return -1 
+            elif i in dp:
+                return dp[i]
+            else:
+                min_count = float("inf")
+                for coin in coins:
+                    count = memo(i + coin)
+                    if count >= 0:
+                        min_count = min(min_count, count + 1)
+                dp[i] = -1 if min_count == float("inf") else min_count
                 return dp[i]
 
-            min_count = float("inf")  
-            
-            for coin in coins:
-                res = memo(i + coin)  
-                if res >= 0: 
-                    min_count = min(min_count, res + 1)  
-
-            dp[i] = -1 if min_count == float("inf") else min_count
-            return dp[i]
-
-        return memo(0) 
+        return memo(0)
