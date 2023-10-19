4e4635825f02eb68de91e1c96adfe4e61ca1bf14
Soumyajit Chakraborty <52927666+Soumyajit567@users.noreply.github.com>
2023-10-18 20:54:05 -0400

    Time: 1441 ms (61.52%), Space: 45.9 MB (56.27%) - LeetHub

--- 2176-parallel-courses-iii/2176-parallel-courses-iii.py
+++ 2176-parallel-courses-iii/2176-parallel-courses-iii.py
@@ -1,3 +1,51 @@
+from collections import defaultdict, deque
+
+class Solution:
+    def minimumTime(self, n: int, relations: List[List[int]], time: List[int]) -> int:
+        # Step 1: Create the graph and in-degree array
+        graph = defaultdict(list)
+        in_degree = {i: 0 for i in range(1, n + 1)}
+        
+        for prevCourse, nextCourse in relations:
+            graph[prevCourse].append(nextCourse)
+            in_degree[nextCourse] += 1
+        
+        # Step 2: Queue for courses that can be taken immediately
+        queue = deque()
+        for course in in_degree:
+            if in_degree[course] == 0:
+                queue.append(course)
+        
+        # Time to complete each course (initially it's the duration of each course itself)
+        completion_time = time.copy()  # here, we're not adding a zero at the front
+        
+        # Step 3: Process the courses
+        while queue:
+            course = queue.popleft()
+            for neighbor in graph[course]:
+                in_degree[neighbor] -= 1
+                # Adjust index by -1 since 'completion_time' is zero-indexed
+                # The time to start a course is the max of the current recorded time and the completion time of its prerequisite
+                completion_time[neighbor - 1] = max(completion_time[neighbor - 1], completion_time[course - 1] + time[neighbor - 1])
+                if in_degree[neighbor] == 0:
+                    queue.append(neighbor)
+        
+        # The result is the maximum completion time among all courses
+        return max(completion_time)
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
 class Solution:
     def minimumTime(self, n: int, relations: List[List[int]], time: List[int]) -> int:
         # Step 1: Create the graph and in-degree array
@@ -29,3 +77,4 @@ class Solution:
         
         # The result is the maximum completion time
         return max(completion_time)
+"""
