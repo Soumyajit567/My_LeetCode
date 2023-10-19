8d2faa48fe1eb667a57282ec5a57231304725a1b
Soumyajit Chakraborty <52927666+Soumyajit567@users.noreply.github.com>
2023-10-19 16:14:06 -0400

    Time: 31 ms (92.96%), Space: 16 MB (97.44%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.py
+++ 0874-backspace-string-compare/0874-backspace-string-compare.py
@@ -1,29 +1,23 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        def next_char(string, index):
-            backspace_count = 0
-            while index >= 0:
-                if string[index] == '#':  # it's a backspace character
-                    backspace_count += 1
-                elif backspace_count > 0:  # it's a character to be deleted
-                    backspace_count -= 1
-                else:
-                    break  # it's a valid character
-                index -= 1  # move to the previous character
-            return index
+        stack = []
+        stack2 = []
+        i = 0
+        j = 0
+        while i < len(s):
+            if s[i] == "#":
+                if stack:
+                    stack.pop()
 
-        i, j = len(s) - 1, len(t) - 1
-        while i >= 0 or j >= 0:
-            i, j = next_char(s, i), next_char(t, j)  # find the next valid character or -1 if none
-            if i < 0 and j < 0:  # reached the start of both strings
-                return True
-            if i < 0 or j < 0:  # reached the start of only one string
-                return False
-            if s[i] != t[j]:  # the characters do not match
-                return False
+            elif s[i] != "#":
+                stack.append(s[i])
+            i += 1
 
-            # move to the previous character
-            i -= 1
-            j -= 1
-
-        return True  # all valid characters matched
+        while j < len(t):
+            if t[j] == "#":
+                if stack2:
+                    stack2.pop()
+            elif t[j] != "#":
+                stack2.append(t[j])
+            j += 1
+        return "".join(stack) == "".join(stack2)
