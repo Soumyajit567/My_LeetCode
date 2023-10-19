aaecd7aa4bfd6027c82afb67dcd6203c40df17be
Soumyajit Chakraborty <52927666+Soumyajit567@users.noreply.github.com>
2023-10-18 21:29:07 -0400

    Time: 74 ms (84.88%), Space: 16.8 MB (40.17%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -3,7 +3,7 @@ class Solution:
         n = len(nums)
         max_prod = min_prod = result = nums[0]
         for i in range(1, n):
-            temp_max = max(max_prod * nums[i], nums[i], min_prod  * nums[i])
+            temp_max  = max(max_prod * nums[i], nums[i], min_prod  * nums[i])
             min_prod = min(nums[i], max_prod * nums[i], min_prod * nums[i])
 
             max_prod = temp_max            
