3f31e4f4df2ad902fc258aeda58543c0dcd2ec1b
Soumyajit Chakraborty <52927666+Soumyajit567@users.noreply.github.com>
2023-10-19 22:26:04 -0400

    Time: 67 ms (35.81%), Space: 16.1 MB (87.79%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.py
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -2,24 +2,20 @@ class Solution:
     def totalNQueens(self, n: int) -> int:
         def can_place(row, col, queens):
             for queen in queens:
-                r, c = queen
-                # Check if two queens are in the same row, column, or diagonal
-                if r == row or c == col or abs(row - r) == abs(col - c):
+                r,c = queen
+                if r == row or c == col or abs(r - row) == abs(c - col):
                     return False
             return True
-
         def backtrack(row, queens):
             if row == n:
-                return 1  # Valid configuration found
-            count = 0
-            for col in range(n):
-                if can_place(row, col, queens):
-                    # Make a move
-                    queens.append((row, col))
-                    # Recurse with this move made
-                    count += backtrack(row + 1, queens)
-                    # Undo the move (backtrack)
-                    queens.pop()
+                return 1
+            else:
+                count = 0 
+                for col in range(n):
+                    if can_place(row, col, queens):
+                        queens.append((row, col))
+                        count += backtrack(row + 1, queens)
+                        queens.pop()
             return count
-
+        
         return backtrack(0, [])
