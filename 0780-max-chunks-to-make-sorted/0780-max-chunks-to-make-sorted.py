243f6f03a6c83b63cf6c48111dafe9b44d8425e3
Soumyajit Chakraborty <52927666+Soumyajit567@users.noreply.github.com>
2023-10-24 00:49:12 -0400

    Time: 46 ms (6.58%), Space: 16.2 MB (42.36%) - LeetHub

---
+++ 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxChunksToSorted(self, arr: List[int]) -> int:
+        n = len(arr)
+        
+        left_max = [0] * n
+        right_min = [float('inf')] * n
+        
+        # Populating left_max
+        left_max[0] = arr[0]
+        for i in range(1, n):
+            left_max[i] = max(left_max[i-1], arr[i])
+        
+        # Populating right_min
+        right_min[-1] = arr[-1]
+        for i in range(n-2, -1, -1):
+            right_min[i] = min(right_min[i+1], arr[i])
+        
+        # Calculating chunks
+        chunks = 1  # At least one chunk: the whole array
+        for i in range(n-1):
+            if left_max[i] <= right_min[i+1]:
+                chunks += 1
+        
+        return chunks
