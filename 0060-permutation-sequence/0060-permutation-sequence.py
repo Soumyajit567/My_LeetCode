2c74d42e5b6995d9130f48cf52d53989ff61a254
Soumyajit Chakraborty <52927666+Soumyajit567@users.noreply.github.com>
2023-11-01 21:23:14 -0400

    Time: 41 ms (53.38%), Space: 16.1 MB (83.97%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.py
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -4,15 +4,18 @@ class Solution:
         factorials = [1]
         for i in range(1, n+1):
             factorials.append(factorials[-1] * i)
+        print(factorials)
             
         # Available numbers to pick from
         numbers = list(range(1, n+1))
-        k -= 1  # Convert k to 0-indexed
+        k -= 1  
         ans = []
-        
+        print(numbers)
         for i in range(n, 0, -1):
             index = k // factorials[i-1]
             ans.append(numbers.pop(index))
+            print(" ")
+            print(ans)
             k %= factorials[i-1]
         
         return ''.join(map(str, ans))
