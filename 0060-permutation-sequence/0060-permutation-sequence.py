0fc76968c8fbc3cd21d8a87be4012ef8a66abffe
Soumyajit Chakraborty <52927666+Soumyajit567@users.noreply.github.com>
2023-11-01 20:18:05 -0400

    Time: 29 ms (97.87%), Space: 16.3 MB (29.12%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -0,0 +1,48 @@
+class Solution:
+    def getPermutation(self, n: int, k: int) -> str:
+        # Calculate factorials for numbers 1 through n
+        factorials = [1]
+        for i in range(1, n+1):
+            factorials.append(factorials[-1] * i)
+            
+        # Available numbers to pick from
+        numbers = list(range(1, n+1))
+        k -= 1  # Convert k to 0-indexed
+        ans = []
+        
+        for i in range(n, 0, -1):
+            index = k // factorials[i-1]
+            ans.append(numbers.pop(index))
+            k %= factorials[i-1]
+        
+        return ''.join(map(str, ans))
+
+
+
+
+
+
+
+
+
+"""
+class Solution:
+    def getPermutation(self, n: int, k: int) -> str:
+        num = "".join([str(i) for i in range(1, n + 1)])
+        res = []
+        def backtrack(num):
+            nonlocal res
+            if len(num) == n: 
+                if len(set(num)) == n:
+                    res.append(int(num))
+                return
+            else:
+                for i in range(1, n + 1):
+                    if str(i) not in num:
+                        backtrack(num + str(i))
+ 
+        for i in range(1, n + 1):
+            backtrack(str(i))
+        res.sort()
+        return str(res[k - 1])
+"""
