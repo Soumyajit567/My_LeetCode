79514d3079e24b4106c710a9afc82762ac974466
Soumyajit Chakraborty <52927666+Soumyajit567@users.noreply.github.com>
2023-11-05 00:10:32 -0400

    Time: 1225 ms (100%), Space: 30.4 MB (100%) - LeetHub

--- 3191-maximum-score-after-applying-operations-on-a-tree/3191-maximum-score-after-applying-operations-on-a-tree.py
+++ 3191-maximum-score-after-applying-operations-on-a-tree/3191-maximum-score-after-applying-operations-on-a-tree.py
@@ -1,20 +1,43 @@
 class Solution:
     def maximumScoreAfterOperations(self, edges: List[List[int]], values: List[int]) -> int:
-        n = len(edges)+1
+        n = len(values)
         graph = [[] for _ in range(n)]
-        for u, v in edges: 
+        for u, v in edges:
             graph[u].append(v)
             graph[v].append(u)
-        
-        def fn(u, p): 
-            """Return """
-            val = s = 0 
-            for v in graph[u]: 
-                if v != p: 
-                    vv, ss = fn(v, u)
-                    val += vv 
-                    s += ss
-            if graph[u] != [p]: val += values[u]
-            return max(val, s), s+values[u]
-        
-        return fn(0, -1)[0]
+
+        # State: (node, parent, is_first_time)
+        stack = [(0, -1, True)]
+        # Stores tuple (val, s) for each node
+        results = {i: (0, 0) for i in range(n)}
+        # Track visited to not reprocess nodes
+        visited = set()
+
+        while stack:
+            node, parent, is_first_time = stack.pop()
+
+            if node in visited:
+                continue
+
+            if is_first_time:
+                # If it's the first time visiting this node, 
+                # we need to process all children before this node
+                stack.append((node, parent, False))  # Add node back to stack to process after children
+                for child in graph[node]:
+                    if child != parent:
+                        stack.append((child, node, True))  # Process children
+            else:
+                # Process node after all children have been processed
+                visited.add(node)
+                val = values[node] if graph[node] != [parent] else 0
+                s = 0
+                for child in graph[node]:
+                    if child != parent:
+                        val += results[child][0]
+                        s += results[child][1]
+                # Save the results
+                results[node] = (max(val, s), s + values[node])
+
+        # The result for the root node will be in results[0]
+        return results[0][0]
+
