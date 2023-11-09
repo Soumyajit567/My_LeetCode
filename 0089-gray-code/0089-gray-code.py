8ea0f1f5c0c3d9b5574d7504dde905250c052666
Soumyajit Chakraborty <52927666+Soumyajit567@users.noreply.github.com>
2023-11-09 00:50:14 -0500

    Time: 94 ms (44%), Space: 23.9 MB (26.59%) - LeetHub

--- 0089-gray-code/0089-gray-code.py
+++ 0089-gray-code/0089-gray-code.py
@@ -2,9 +2,9 @@ class Solution:
     def grayCode(self, n: int) -> List[int]:
         res = []
         res.append(0)
-        for i in range(n):
+        for i in range(1, n + 1):
             prev_len = len(res)
-            prev = i
+            prev = i - 1
             mask = 1 << prev
             for j in range(prev_len - 1, -1, -1):
                 res.append((mask | res[j]))
