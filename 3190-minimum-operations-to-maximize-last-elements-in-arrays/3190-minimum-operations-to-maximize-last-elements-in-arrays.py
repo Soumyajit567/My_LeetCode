a7ac79e0914c60064bcb4b06d25fa2fe6995b1b5
Soumyajit Chakraborty <52927666+Soumyajit567@users.noreply.github.com>
2023-11-11 23:03:27 -0500

    Time: 194 ms (100%), Space: 16.7 MB (100%) - LeetHub

---
+++ 3190-minimum-operations-to-maximize-last-elements-in-arrays/3190-minimum-operations-to-maximize-last-elements-in-arrays.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minOperations(self, nums1: List[int], nums2: List[int]) -> int:
+        def check(a, b):
+                count, mx1, mx2 = 0, 0, 0
+                for x, y in zip(nums1[:-1], nums2[:-1]):
+                        if a >= max(mx1, x) and b>=max(mx2, y):
+                                mx1, mx2 = max(mx1, x), max(mx2, y)
+                        elif a >= max(mx1, y) and b>=max(mx2, x):
+                                mx1, mx2 = max(mx1, y), max(mx2, x)
+                                count += 1
+                        else:
+                                return len(nums1)+10
+                return count
+        ans = min(check(nums1[-1], nums2[-1]), check(nums2[-1], nums1[-1])+1)
+        return ans if ans<len(nums1) else -1
