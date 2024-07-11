65a4046dca285422f3bef475651de217bc10ecf5
Soumyajit Chakraborty <52927666+Soumyajit567@users.noreply.github.com>
2024-07-10 20:00:41 -0400

    Time: 854 ms (42.79%), Space: 38.8 MB (34.06%) - LeetHub

--- 0819-minimum-swaps-to-make-sequences-increasing/0819-minimum-swaps-to-make-sequences-increasing.py
+++ 0819-minimum-swaps-to-make-sequences-increasing/0819-minimum-swaps-to-make-sequences-increasing.py
@@ -1,19 +1,16 @@
 class Solution:
     def minSwap(self, nums1: List[int], nums2: List[int]) -> int:
-        n = len(nums1)
-        no_swap = [float('inf')] * n  
-        swap = [float('inf')] * n  
-
-        no_swap[0] = 0
-        swap[0] = 1
-
-        for i in range(1, n):
-            if nums1[i] > nums1[i-1] and nums2[i] > nums2[i-1]:
-                no_swap[i] = no_swap[i-1]
-                swap[i] = swap[i-1] + 1
+        maxnum=len(nums1)+1
+        dp=[[maxnum,maxnum] for k in range(len(nums1))]
+        dp[0][0],dp[0][1]=0,1
+        for i in range(1,len(nums1)):
             
-            if nums1[i] > nums2[i-1] and nums2[i] > nums1[i-1]:
-                no_swap[i] = min(no_swap[i], swap[i-1])  
-                swap[i] = min(swap[i], no_swap[i-1] + 1)  
-
-        return min(no_swap[-1], swap[-1])
+            if nums1[i]>nums1[i-1] and nums2[i]>nums2[i-1]:
+                dp[i][0]=min(dp[i][0],dp[i-1][0])
+                dp[i][1]=min(dp[i][1],dp[i-1][1]+1)
+            if nums1[i]>nums2[i-1] and nums2[i]>nums1[i-1]:
+                dp[i][0]=min(dp[i][0],dp[i-1][1])
+                dp[i][1]=min(dp[i][1],dp[i-1][0]+1)
+            if dp[i][0]==maxnum or dp[i][1]==maxnum:
+                return -1
+        return min(dp[len(nums1)-1][0],dp[len(nums1)-1][1])
