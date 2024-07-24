527b86a60ebadec7c6961119f4f2f46f86f56f2f
Soumyajit Chakraborty <52927666+Soumyajit567@users.noreply.github.com>
2024-07-24 09:08:54 -0400

    Time: 875 ms (48.42%), Space: 31.4 MB (29.85%) - LeetHub

--- 3498-minimum-array-changes-to-make-differences-equal/3498-minimum-array-changes-to-make-differences-equal.py
+++ 3498-minimum-array-changes-to-make-differences-equal/3498-minimum-array-changes-to-make-differences-equal.py
@@ -1,30 +1,18 @@
 class Solution:
     def minChanges(self, nums: List[int], k: int) -> int:
-        n = len(nums)
-        n_diffs = math.ceil(n / 2)
+        max_deltas, deltas = [], []
 
-        # Find the counts for the symetrical differences.        
-        diff_counts = defaultdict(int)
-        for i in range(n_diffs):
-            diff = abs(nums[i] - nums[~i])
-            diff_counts[diff] += 1
+        for i in range(len(nums) // 2):
+            max_delta = max(nums[i], nums[~i], k - min(nums[i], nums[~i]))
+            max_deltas.append(max_delta)
+            deltas.append(abs(nums[i] - nums[~i]))
 
-        # Find the counts for the cost 1 thresholds
-        cost_1_threshold_counts = defaultdict(int)
-        for i in range(n_diffs):
-            l = min(nums[i], nums[~i])
-            h = max(nums[i], nums[~i])
-            cost_1_threshold = max(h, k-l)
-            cost_1_threshold_counts[cost_1_threshold] += 1
+        max_deltas.sort()
+        counter = Counter(deltas)
+        changes = float("inf")
 
-        # Calculate the cost for each possible value of x.
-        min_cost = math.inf
-        n_cost_2 = 0
-        for x in range(k+1):
-            cur_cost = n_diffs + n_cost_2 - diff_counts[x] 
-
-            min_cost = min(min_cost, cur_cost)
-
-            n_cost_2 += cost_1_threshold_counts[x]
-        
-        return min_cost
+        for target_delta, count in counter.items():
+            ones_start = bisect_left(max_deltas, target_delta)
+            cur_changes = -count + ones_start * 2 + (len(max_deltas) - ones_start)
+            changes = min(changes, cur_changes)
+        return changes
