e312f68ee3f2fdbd1d12860cc807a1c7298167f9
Soumyajit Chakraborty <52927666+Soumyajit567@users.noreply.github.com>
2024-07-24 21:14:37 -0400

    Time: 1329 ms (14.97%), Space: 44 MB (5.01%) - LeetHub

--- 3498-minimum-array-changes-to-make-differences-equal/3498-minimum-array-changes-to-make-differences-equal.py
+++ 3498-minimum-array-changes-to-make-differences-equal/3498-minimum-array-changes-to-make-differences-equal.py
@@ -1,18 +1,33 @@
 class Solution:
     def minChanges(self, nums: List[int], k: int) -> int:
-        max_deltas, deltas = [], []
-
-        for i in range(len(nums) // 2):
-            max_delta = max(nums[i], nums[~i], k - min(nums[i], nums[~i]))
-            max_deltas.append(max_delta)
-            deltas.append(abs(nums[i] - nums[~i]))
-
-        max_deltas.sort()
-        counter = Counter(deltas)
-        changes = float("inf")
-
-        for target_delta, count in counter.items():
-            ones_start = bisect_left(max_deltas, target_delta)
-            cur_changes = -count + ones_start * 2 + (len(max_deltas) - ones_start)
-            changes = min(changes, cur_changes)
-        return changes
+        n = len(nums)
+        mid = n // 2
+        f = collections.defaultdict(int)
+        f2 = collections.defaultdict(int)
+        i = 0
+        while i < mid:
+            diff = abs(nums[i] - nums[~i]) 
+            if diff not in f:
+                f[diff] = 1
+            else:
+                f[diff] += 1
+            left_n = nums[i]
+            comp_left = k - nums[i]
+            right_n = nums[~i]
+            comp_right = k - nums[~i]
+            max_left = max(left_n, comp_left)
+            max_right = max(right_n, comp_right)
+            key = max(max_left, max_right)
+            if key not in f2:
+                f2[key] = 1
+            else:
+                f2[key] += 1
+            i += 1
+        MAX = 10 ** 5
+        count = 0
+        best = MAX
+        for i in range(k + 1):
+            if i - 1 >= 0:
+                count += f2[i - 1]
+            best = min(best, count + (mid - f[i]))
+        return best
