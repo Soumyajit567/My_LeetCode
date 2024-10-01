f7e13b0744ebe25961e51e30308aaf7d2e0bef69
Soumyajit Chakraborty <52927666+Soumyajit567@users.noreply.github.com>
2024-10-01 06:22:17 -0400

    Time: 520 ms (78.29%), Space: 30.6 MB (39.43%) - LeetHub

--- 2301-count-array-pairs-divisible-by-k/2301-count-array-pairs-divisible-by-k.py
+++ 2301-count-array-pairs-divisible-by-k/2301-count-array-pairs-divisible-by-k.py
@@ -1,59 +1,37 @@
 class Solution:
     def countPairs(self, nums: List[int], k: int) -> int:
-        def prime_factors(n):
-            i = 2
-            factors = defaultdict(int)
-            while i * i <= n:
-                count = 0
-                while n % i == 0:
-                    n //= i
-                    count += 1
-                if count > 0:
-                    factors[i] = count
-                i += 1
-            if n > 1:
-                factors[n] += 1
-            return factors
-
-        k_factors = prime_factors(k)
-        primes = list(k_factors.keys())
-        k_exponents = [k_factors[p] for p in primes]
-        m = len(primes)
-
-        def get_exponent_profile(num):
-            exponents = []
-            for i in range(m):
-                p = primes[i]
-                e = 0
-                while num % p == 0:
-                    num //= p
-                    e += 1
-                exponents.append(min(e, k_exponents[i]))
-            return tuple(exponents)
-
-        counts = defaultdict(int)
-        exponent_profiles = []
-
+        divisors = []
+        for i in range(1, int(k**0.5) + 1):
+            if k % i == 0:
+                divisors.append(i)
+                if i != k // i:
+                    divisors.append(k // i)
+        divisors.sort()
+        divisor_indices = {d: i for i, d in enumerate(divisors)}
+        
+        # Step 2: Count occurrences of gcds
+        counts = [0] * len(divisors)
         for num in nums:
-            ep = get_exponent_profile(num)
-            exponent_profiles.append(ep)
-            counts[ep] += 1
-
-
-        total_pairs = 0
-        exponent_profiles_set = list(counts.keys())
-
-        for i in range(len(exponent_profiles_set)):
-            ep1 = exponent_profiles_set[i]
-            counts_ep1 = counts[ep1]
-            for j in range(i, len(exponent_profiles_set)):
-                ep2 = exponent_profiles_set[j]
-                counts_ep2 = counts[ep2]
-                sum_exponents = [ep1[x] + ep2[x] for x in range(m)]
-                if all(sum_exponents[x] >= k_exponents[x] for x in range(m)):
+            num_gcd = gcd(num, k)
+            idx = divisor_indices[num_gcd]
+            counts[idx] += 1
+        
+        # Step 3: Count valid pairs
+        res = 0
+        for i in range(len(divisors)):
+            d1 = divisors[i]
+            c1 = counts[i]
+            if c1 == 0:
+                continue
+            for j in range(i, len(divisors)):
+                d2 = divisors[j]
+                c2 = counts[j]
+                if c2 == 0:
+                    continue
+                if (d1 * d2) % k == 0:
                     if i == j:
-                        total_pairs += counts_ep1 * (counts_ep1 - 1) // 2
+                        # Avoid double counting when d1 == d2
+                        res += c1 * (c1 - 1) // 2
                     else:
-                        total_pairs += counts_ep1 * counts_ep2
-
-        return total_pairs
+                        res += c1 * c2
+        return res
