3ab56ced434b706861942e87851bb98332d2459c
Soumyajit Chakraborty <52927666+Soumyajit567@users.noreply.github.com>
2024-11-07 07:29:57 -0500

    Time: 163 ms (29.03%), Space: 16.6 MB (86.93%) - LeetHub

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.py
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.py
@@ -1,18 +1,17 @@
 class Solution:
     def maxUniqueSplit(self, s: str) -> int:
-        n = len(s)
-        max_len = 0
-        unique = set()
-        stack = [(0, set())]
-        while stack:
-            start, unique_substrings = stack.pop()
-            if start == n:
-                max_len = max(max_len, len(unique_substrings))
-                continue
-            else:
-                for end in range(start + 1, n + 1):
-                    sub = s[start:end]
-                    if sub not in unique_substrings:
-                        new_unique_substrings = unique_substrings | {sub}
-                        stack.append((end, new_unique_substrings))
-        return max_len
+        def backtrack(start, unique_substrings):
+            if start == len(s):
+                return len(unique_substrings)
+            
+            max_splits = 0
+            for end in range(start + 1, len(s) + 1):
+                substring = s[start:end]
+                if substring not in unique_substrings:
+                    unique_substrings.add(substring)
+                    max_splits = max(max_splits, backtrack(end, unique_substrings))
+                    unique_substrings.remove(substring)
+            
+            return max_splits
+        
+        return backtrack(0, set())
