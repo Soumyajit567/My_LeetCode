db0e162e99b0d9eec43566a5eceaf29961566a14
Soumyajit Chakraborty <52927666+Soumyajit567@users.noreply.github.com>
2024-11-25 23:00:22 -0500

    Time: 15 ms (32.35%), Space: 30.1 MB (97.58%) - LeetHub

--- 1679-shortest-subarray-to-be-removed-to-make-array-sorted/1679-shortest-subarray-to-be-removed-to-make-array-sorted.py
+++ 1679-shortest-subarray-to-be-removed-to-make-array-sorted/1679-shortest-subarray-to-be-removed-to-make-array-sorted.py
@@ -1,31 +1,20 @@
 class Solution:
     def findLengthOfShortestSubarray(self, arr: List[int]) -> int:
         n = len(arr)
-        
-        # Find the longest non-decreasing prefix
         left = 0
         while left < n - 1 and arr[left] <= arr[left + 1]:
             left += 1
-        
-        # If the entire array is sorted
         if left == n - 1:
             return 0
-        
-        # Find the longest non-decreasing suffix
         right = n - 1
-        while right > 0 and arr[right - 1] <= arr[right]:
+        while right > 0 and arr[right] >= arr[right - 1]:
             right -= 1
-        
-        # Minimum removal if we remove all elements to the left or right
-        result = min(n - left - 1, right)
-        
-        # Try merging the prefix and suffix
+        res = min(n - left - 1, right)
         i, j = 0, right
         while i <= left and j < n:
             if arr[i] <= arr[j]:
-                result = min(result, j - i - 1)
+                res = min(res, j - i - 1)
                 i += 1
             else:
                 j += 1
-        
-        return result
+        return res
