ab80550736fb0252636bad5440121f5412fb5703
Soumyajit Chakraborty <52927666+Soumyajit567@users.noreply.github.com>
2024-12-04 23:56:39 -0500

    Time: 1512 ms (53.52%), Space: 30.4 MB (26.48%) - LeetHub

--- 3594-identify-the-largest-outlier-in-an-array/3594-identify-the-largest-outlier-in-an-array.py
+++ 3594-identify-the-largest-outlier-in-an-array/3594-identify-the-largest-outlier-in-an-array.py
@@ -1,14 +1,15 @@
 class Solution:
     def getLargestOutlier(self, nums: List[int]) -> int:
-        s = collections.defaultdict(int)
+        hashmap = collections.defaultdict(int)
+        for idx, val in enumerate(nums):
+            hashmap[val] = idx
+        total_sum = sum(nums)
+        res = float("-inf")
         for i, v in enumerate(nums):
-            s[v] = i
-        total = sum(nums)
-        res = float('-inf')
-        for i,num in enumerate(nums):
-            curr_sum = total - num
-            if curr_sum%2 == 0: 
+            curr_sum = total_sum - v
+            if curr_sum % 2 == 0:
                 val = curr_sum // 2
-                if val in s and s[val] != i:
-                    res = max(res, num)
+                if val in hashmap and hashmap[val] != i:
+                    res = max(res, v)
         return res
+
