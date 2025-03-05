7cc44dc6102452a878e090f3dff6d57d6d4a2e16
Soumyajit Chakraborty <52927666+Soumyajit567@users.noreply.github.com>
2025-03-05 16:14:19 -0500

    Time: 447 ms (5.43%), Space: 18 MB (7.34%) - LeetHub

--- 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.py
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.py
@@ -1,14 +1,14 @@
 class Solution:
     def checkPowersOfThree(self, n: int) -> bool:
         p = set()
-        
+        limit = math.floor(math.log10(10**7) / math.log10(3))
         def backtrack(i, _sum, p):
             if _sum == n:
                 return True
             if _sum > n:
                 return False
             
-            for k in range(i, 20):  
+            for k in range(i, limit + 1):  
                 if k not in p:
                     p.add(k)
                     if backtrack(k + 1, _sum + pow(3, k), p):
