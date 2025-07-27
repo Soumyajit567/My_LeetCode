04fcb6917081c968733e868bd30a6bb008fa4fec
Soumyajit Chakraborty <52927666+Soumyajit567@users.noreply.github.com>
2025-07-27 16:51:51 +0530

    Time: 916 ms (27.57%), Space: 72.2 MB (5.13%) - LeetHub

--- 2588-maximum-number-of-points-from-grid-queries/2588-maximum-number-of-points-from-grid-queries.py
+++ 2588-maximum-number-of-points-from-grid-queries/2588-maximum-number-of-points-from-grid-queries.py
@@ -1,24 +1,55 @@
 class Solution:
     def maxPoints(self, grid: List[List[int]], queries: List[int]) -> List[int]:
-        M = len(grid)
-        N = len(grid[0])
-        q = [(n, i) for i, n in enumerate(queries)]
-        q.sort()
-        visited = set()
-        res = [0 for i in range(len(queries))]
-        minHeap = [(grid[0][0], 0 , 0)]
-        points = 0
-        visited.add((0,0))
-        directions = [(-1, 0),(0, -1), (1, 0), (0, 1)]
-        for limit, idx in q:
-            while minHeap and minHeap[0][0] < limit:
-                val, r, c = heapq.heappop(minHeap)
-                points += 1
-                for dx, dy in directions:
-                    dr = dx + r
-                    dc = dy + c
-                    if 0 <= dr < M and 0 <= dc < N and (dr,dc) not in visited:
-                        heapq.heappush(minHeap,(grid[dr][dc], dr, dc))
-                        visited.add((dr,dc))
-            res[idx] = points
-        return res
+        M, N = len(grid), len(grid[0])
+        parent = {}   # Union-Find parent
+        size = {}     # Size of each component
+        result = [0] * len(queries)
+
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
+
+        def union(x, y):
+            xr, yr = find(x), find(y)
+            if xr == yr:
+                return
+            if size[xr] < size[yr]:
+                xr, yr = yr, xr
+            parent[yr] = xr
+            size[xr] += size[yr]
+
+        # Flatten and sort grid cells by value
+        cells = [(grid[r][c], r, c) for r in range(M) for c in range(N)]
+        cells.sort()
+
+        # Sort queries and track original indices
+        sorted_queries = sorted((q, i) for i, q in enumerate(queries))
+
+        visited = [[False] * N for _ in range(M)]
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+        cell_idx = 0
+        for q, qi in sorted_queries:
+            # Add all cells < q into union-find
+            while cell_idx < len(cells) and cells[cell_idx][0] < q:
+                val, r, c = cells[cell_idx]
+                idx = r * N + c
+                parent[idx] = idx
+                size[idx] = 1
+                visited[r][c] = True
+                # Try to union with 4 neighbors
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc
+                    if 0 <= nr < M and 0 <= nc < N and visited[nr][nc]:
+                        neighbor_idx = nr * N + nc
+                        union(idx, neighbor_idx)
+                cell_idx += 1
+
+            # Check if (0, 0) is added to any component
+            if visited[0][0]:
+                result[qi] = size[find(0)]
+            else:
+                result[qi] = 0
+
+        return result
