f55cc8859675fcd017823c3123b52911b4beae4e
Soumyajit Chakraborty <52927666+Soumyajit567@users.noreply.github.com>
2025-07-28 22:14:38 +0530

    Time: 2771 ms (5.09%), Space: 17.7 MB (90.63%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.py
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.py
@@ -2,23 +2,20 @@ class Solution:
     def countMaxOrSubsets(self, nums: List[int]) -> int:
         n = len(nums)
         max_or = 0
-        
-        # Calculate the maximum bitwise OR by iterating through all subsets
         for i in range(1 << n):
-            current_or = 0
+            current_or  = 0
             for j in range(n):
                 if (i & (1 << j)) != 0:
                     current_or |= nums[j]
-            max_or = max(max_or, current_or)
-        
-        # Count the number of subsets that have the maximum bitwise OR
+            max_or = max(current_or, max_or)
+
         count = 0
+        
         for i in range(1 << n):
             current_or = 0
             for j in range(n):
                 if (i & (1 << j)) != 0:
                     current_or |= nums[j]
-            if current_or == max_or:
+            if max_or == current_or:
                 count += 1
-                
         return count
