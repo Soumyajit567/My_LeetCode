3ae0a65424a3deb900904ac631be4f0139123603
Soumyajit Chakraborty <52927666+Soumyajit567@users.noreply.github.com>
2025-07-29 16:19:39 +0530

    Time: 27 ms (92.59%), Space: 18.1 MB (27.78%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.py
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.py
@@ -14,18 +14,22 @@ class Solution:
         for i in range(1 << len(left)):
             current_or = 0
             for j in range(len(left)):
-                if i & (1 << j):
+                if i & (1 << j)!= 0:
                     current_or |= left[j]
-            left_ors[current_or] += 1
+            left_ors[current_or] += 1 
+        print(left_ors)
+        print("     ")
         
         # Compute OR values and their counts for right half
         right_ors = defaultdict(int)
         for i in range(1 << len(right)):
             current_or = 0
             for j in range(len(right)):
-                if i & (1 << j):
+                if i & (1 << j)!= 0:
                     current_or |= right[j]
             right_ors[current_or] += 1
+        print(right_ors)
+        print("       ")
         
         # Find maximum OR by combining left and right
         max_or = 0
@@ -35,9 +39,10 @@ class Solution:
         
         # Count subsets with maximum OR
         count = 0
-        for l in left_ors:
-            for r in right_ors:
+        for l in left_ors.keys():
+            for r in right_ors.keys():
                 if (l | r) == max_or:
                     count += left_ors[l] * right_ors[r]
+                    print(count)
         
         return count
